e0556e579080c27b57fa050012c3becbcfa3ff7a
Letionc <libaichuanphigros@126.com>
2025-04-13 13:12:56 +0800

    Update update.js

--- luaEg/update.js
+++ luaEg/update.js
@@ -1,7 +1,14 @@
-fetch("https://letionc.github.io/luaEg/1.lua")
-  .then(a=>{
-    return a.text()
-  })
-  .then(a=>{
-    document.getElementById("eg1").innerHTML=a;
-  })
+"use strict";
+var i=0;
+while(++i){
+  fetch(`https://letionc.github.io/luaEg/{i}.lua`)
+    .then(a=>{
+      return a.text();
+    })
+    .then(a=>{
+      document.getElementById("eg{i}").innerHTML=a;
+    })
+    .catch(e==>{
+        break;
+    });
+};
