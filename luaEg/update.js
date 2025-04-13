87153d5dd6fa27515a25e8c86e9f9e6a8d1f45c3
Letionc <libaichuanphigros@126.com>
2025-04-13 13:39:25 +0800

    fix bug

--- luaEg/update.js
+++ luaEg/update.js
@@ -1,12 +1,15 @@
 "use strict";
 var i=0;
 while(++i){
-  fetch(`https://letionc.github.io/luaEg/{i}.lua`)
+  fetch(`https://letionc.github.io/luaEg/${i}.lua`)
     .then(a=>{
       return a.text();
     })
     .then(a=>{
-      document.getElementById("eg{i}").innerHTML=a;
+      nw=document.createElement('code');
+      nw.innerHTML=a;
+      nw.class="lang-lua";
+      document.getElementById('codes').appendChild(nw);
     })
     .catch(e=>{
         i=-1;
