4ba0260232f529838c9cea8c5a08f136ab1dc89a
Letionc <libaichuanphigros@126.com>
2025-04-20 22:53:23 +0800

    Update update.js

--- luaEg/update.js
+++ luaEg/update.js
@@ -1,9 +1,7 @@
 "use strict";
 for(var i=1;i<=1;i++){
   fetch(`https://letionc.github.io/luaEg/${i}.lua`)
-    .then(a=>{
-      return a.text();
-    })
+    .then(a=>a.text())
     .then(a=>{
       document.getElementById("eg"+i).innerHTML=a;
     });
