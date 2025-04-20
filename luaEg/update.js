37382253b84a06847ea249105dcfcd2bc0c70a78
Letionc <libaichuanphigros@126.com>
2025-04-20 23:01:48 +0800

    fix bug

--- luaEg/update.js
+++ luaEg/update.js
@@ -1,8 +1,11 @@
 "use strict";
-setTimeout(()=>for(var i=1;i<=1;i++){
-  fetch(`https://letionc.github.io/luaEg/${i}.lua`)
-    .then(a=>a.text())
-    .then(a=>{
-      document.getElementById("eg"+i).innerHTML=a;
-    });
-},50)
+setTimeout(()=>{
+  for(var i=1;i<=1;i++){
+    fetch(`https://letionc.github.io/luaEg/${i}.lua`)
+      .then(a=>a.text())
+      .then(a=>{
+        document.getElementById("eg"+i).innerHTML=a;
+      }
+      );
+  }
+},50);
