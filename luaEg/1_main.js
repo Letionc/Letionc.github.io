6d2ce61bad71a9319945f0044b131ca69efa5b52
Letionc <libaichuanphigros@126.com>
2025-04-26 22:41:25 +0800

    fuck this update 1_main.js

--- luaEg/1_main.js
+++ luaEg/1_main.js
@@ -1,16 +1,26 @@
 getItemContents=getItemNames;
 //有了物品描述后需要删掉第一行代码
+//以后要用concat合并数组了 啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
+var ItemNames1=getItemNames(),
+  ItemContents1=getItemContents(),
+  a=[],
+  b=[];
+setTimeout(function(){
+  for(let x in ItemNames1){
+    if(ItemNames1[x]!=''){
+      a.push(ItemNames1[x]);
+      b.push(ItemContents1[x]);
+    }
+  }
+},0);
 function search(){
-  var a=getItemNames(),
-    b=getItemContents(),
-    f=document.getElementById('in').value;
-    var e=document.createElement('tbody');
+    var f=document.getElementById('in').value,
+      e=document.createElement('tbody');
     document.getElementById('table').remove();
     document.getElementById('choose').appendChild(e);
     e.id="table";
   if(f=="")return;
   for(let x in a){
-    if(a[x]=='')continue;
     if(!(`${x}`.includes(f)||a[x].includes(f)||b[x].includes(f)))continue;
     var e=document.createElement('tr'),
       e1=document.createElement('td'),
