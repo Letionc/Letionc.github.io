a7d7430793f29c7a321380ddcd6a405a3cceb773
Letionc <libaichuanphigros@126.com>
2026-08-03 21:01:59 +0800

    fix jsRunner/app.js

--- jsRunner/app.js
+++ jsRunner/app.js
@@ -1,10 +1,24 @@
 var a = document.getElementById('1');
+
 document.getElementById('3').addEventListener("click", function (e) {
     try {
-        this.innerText = String(eval(a.innerText));
-        this.style.color = '#0f0'
+        this.innerText = String(eval(a.value));
+        this.style.color = '#396'
     } catch (err) {
         this.innerText = err.message;
-        this.style.color = '#f00'
+        this.style.color = '#933'
     }
 }.bind(document.getElementById('2')))
+
+document.getElementById('2').addEventListener("keydown", function (e) {
+	if(e.ctrlKey && e.code == "Enter"){
+        try {
+            this.innerText = String(eval(a.value));
+            this.style.color = '#396'
+        } catch (err) {
+            this.innerText = err.message;
+            this.style.color = '#933'
+        }
+    }
+}.bind(document.getElementById('2')))
+    
