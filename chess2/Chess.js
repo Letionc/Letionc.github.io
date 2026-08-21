83eff416271562541458382aee3a66335fb1a281
Letionc <libaichuanphigros@126.com>
2026-08-21 22:27:10 +0800

    add /chess2/*.js
    why do i do it later

---
+++ chess2/Chess.js
@@ -0,0 +1,160 @@
+const CHESS_DOMCREATE_DEFAULT_CONFIG = {
+	w: e => {
+		e.style.background = '#fff';
+		e.style.borderRadius = '10px';
+	},
+	b: e => {
+		e.style.background = '#000';
+		e.style.borderRadius = '10px';
+	},
+	[null]: e => {
+		e.style.background = '#888';
+		e.style.borderRadius = '0';
+	}
+};
+function createGrid(h, w, val){
+	return new Array(h).fill(e => new Array(w).fill(val)).map(f => f());
+}
+class Chess{
+	constructor(h = 0, w = 0, wincnt = 5, checker = [[0,1],[1,1],[1,0],[1,-1]]){
+		this.win = null;
+		this.table = createGrid(h, w, null);
+		this.h = h, this.w = w, this.wincnt = wincnt, this.checker = checker;
+	}
+	toString(){
+		return JSON.stringify({winner: this.win, table: this.table});
+	}
+	getWinner(){
+		return this.win;
+	}
+	getGrid(){
+		return this.table;
+	}
+	
+	init(h = 0, w = 0, wincnt = 5, checker = [[0,1],[1,1],[1,0],[1,-1]]){
+		this.win = null;
+		this.table = createGrid(h, w, null);
+		this.h = h, this.w = w, this.wincnt = wincnt, this.checker = checker;
+	}
+	isIn(y, x){
+		return (y >= 0) && (y < this.h) && (x >= 0) && (x < this.w);
+	}
+	
+	getc(y, x){
+		return this.isIn(y, x)? this.table[y][x]: undefined;
+	}
+	check(y, x, ch, dy = 1, dx = 0, cnt = 5){
+		var now = 0;
+		for(let i = -cnt; i <= cnt; ++i){
+			if(this.getc(y+dy*i, x+dx*i) === ch){
+				++now;
+			}else{
+				now = 0;
+			}
+			if(now >= cnt){
+				return true;
+			}
+		}
+		return now >= cnt;
+	}
+	down(y, x, ch){
+		if(this.win !== null){
+			return {status: 'error', msg: "ENDV"};
+		}
+		if(!this.isIn(y, x)){
+			return {status: 'error', msg: "OUTP"};
+		}
+		if(this.table[y][x] !== null){
+			return {status: 'error', msg: "USED"};
+		}
+		this.table[y][x] = ch;
+		
+		for(const e of this.checker){
+			if(this.check(y, x, ch, e[0], e[1], this.wincnt)){
+				this.win = ch;
+				return {status: 'ok', winner: ch};
+			}
+		}
+		
+		return {status: 'ok', winner: this.win};
+	}
+	up(y, x){
+		if(!this.isIn(y, x)){
+			return {status: 'error', msg: "OUTP"};
+		}
+		if(this.table[y][x] === null){
+			return {status: 'error', msg: "EMTY"};
+		}
+		const v = this.table[y][x];
+		this.table[y][x] = null;
+		
+		if(this.win === null) return undefined;
+		for(const e of this.checker){
+			if(this.check(e[0], e[1], v)){
+				return {status: 'ok', winner: this.win};
+			}
+		}
+		this.win = null;
+		
+		return {status: 'ok', winner: null};
+	}
+	
+	static domCreate(config = CHESS_DOMCREATE_DEFAULT_CONFIG){
+		const elem_grid = document.createElement('div');
+		elem_grid.style.height = "fit-content";
+		elem_grid.style.width = "fit-content";
+		elem_grid.style.borderCollapse = "collapse";
+		elem_grid.style.border = "1px solid #000";
+		elem_grid.style.display = "block";
+		elem_grid.style.padding = "0";
+		for(const row of this.table){
+			const elem_row = document.createElement('div');
+			elem_row.style.height = "fit-content";
+			elem_row.style.width = "fit-content";
+			elem_row.style.borderCollapse = "collapse";
+			elem_row.style.border = "1px solid #000";
+			elem_row.style.display = "block";
+			elem_row.style.margin = "0";
+			elem_row.style.padding = "0";
+			for(const block of row){
+				const elem_block = document.createElement('div');
+				elem_block.style.width = "20px";
+				elem_block.style.height = "20px";
+				elem_block.style.border = "1px solid #000";
+				elem_block.style.display = "inline-block";
+				elem_block.style.margin = "0";
+				elem_block.style.textAlign = "center";
+				elem_block.style.color = "#888";
+				elem_block.style.background = "#fff";
+				elem_block.dataset.value = block;
+				(f=>f||(e=>e.innerHTML=''))(config[block])(elem_block);
+				elem_row.appendChild(elem_block);
+			}
+			elem_grid.appendChild(elem_row);
+		}
+		return elem_grid;
+	}
+	static domGetP(elem_grid, y, x){
+		return elem_grid.children[y].children[x];
+	}
+	domUpdateP(y, x, elem_block, config = CHESS_DOMCREATE_DEFAULT_CONFIG){
+		(f=>f||(e=>e.innerHTML=''))(config[this.getc(y, x)])(elem_block);
+		return;
+	}
+	domUpdateAll(elem_grid, config = CHESS_DOMCREATE_DEFAULT_CONFIG){
+		Array.from(elem_grid.children).forEach(
+			(r,y) => Array.from(r.children).forEach(
+				(e,x) => this.domUpdateP(y, x, e, config)
+			)
+		);
+	}
+	static domListen(elem_grid, type, listener){
+		Array.from(elem_grid.children).forEach(
+			(r,y) => Array.from(r.children).forEach(
+				(e,x) => e.addEventListener(type, event => listener(event, e, y, x))
+			)
+		);
+		return;
+	}
+}
+
---
+++ chess2/app.js
@@ -0,0 +1,118 @@
+function chessCreate(handlers, h = 15, w = 15, wincnt = 5, checker = [[0,1],[1,1],[1,0],[1,-1]]){
+	const chess = new Chess(h, w, wincnt, checker);
+	const chess_dom = chess.domCreate();
+	var cnt = 0;
+	const res = {
+		chess: chess,
+		dom: chess_dom,
+		init: (h, w, wincnt, checker) => (cnt=0, chess.init(h, w, wincnt, checker)),
+		getWinner: chess.getWinner,
+	};
+	handlers = handlers || {};
+	handlers.down = handlers.down || ret => console.log("chess.down => "+String(ret));
+	handlers.up = handlers.up || ret => console.log("chess.up => "+String(ret));
+	
+	chess.domListen(chess_dom, "click", (_,e,y,x) => {
+		const ret = chess.down(y, x, (cnt++) & 1? "w": "b");
+		chess.domUpdateP(y, x, e);
+		handlers.down(ret);
+	});
+	chess.domListen(chess_dom, "countextmenu", (_,e,y,x) => {
+		const ret = chess.up(y, x);
+		chess.domUpdateP(y, x, e);
+		handlers.up(ret);
+	});
+	
+	return res;
+}
+/*
+
+<label>宽：<input type="number" min="5" step="1" value="15" placeholder="棋盘宽度，例如 15" />
+</label>
+<label>高：<input type="number" min="5" step="1" value="15" placeholder="棋盘高度，例如 15" />
+</label>
+<label>胜利需要几子棋：<input type="number" min="5" step="1" value="5" placeholder="填 4 就是 四子棋" />
+</label>
+<br />
+<label>胜利检查的方向：<input type="number" min="5" step="1" value="[[0,1],[1,1],[1,0],[1,-1]]" placeholder="以这个 [dx,dy] 列表检查" />
+</label>
+<br />
+<button>初始化</button>
+<p>操作返回值：null</p>
+<p>胜利者：null</p>
+
+*/
+// (e=>(,e))(document.createElement(""))
+document.getElementById("btn-create-instance").addEventListener("click", event => {
+	const config = {
+		w: +document.getElementById("inp-w").value,
+		h: +document.getElementById("inp-h").value,
+		wincnt: +document.getElementById("inp-wincnt").value,
+		checker: JSON.parse(document.getElementById("inp-checker").value),
+	};
+	const p_status = (e=>(
+		e.innerHTML = '操作返回值：null',
+		e
+	))(document.createElement("p"));
+	const p_winner = (e=>(
+		e.innerHTML = '获胜者：null',
+		e
+	))(document.createElement("p"));
+	
+	const handlers = {
+		down: ret => {
+			const status = ret.status;
+			p_status.innerText = `操作返回值：${JSON.stringify(ret)}`;
+			if(status == "ok"){
+				const transtab = {b:"黑", w:'白', [null]:"null"};
+				p_winner.innerText = `获胜者：${transtab[ret.winner]}`;
+			}
+		},
+		up: ret => {
+			const status = ret.status;
+			p_status.innerText = `操作返回值：${JSON.stringify(ret)}`;
+			if(status == "ok"){
+				const transtab = {b:"黑", w:'白', [null]:"null"};
+				p_winner.innerText = `获胜者：${transtab[ret.winner]}`;
+			}
+		},
+	};
+	const res = chessCreate(handlers, config.w, config.h, config.wincnt, config.checker);
+	// const label_w = (e=>(
+		// e.innerHTML = '宽：<input type="number" min="5" step="1" value="15" placeholder="棋盘宽度，例如 15" disabled />',
+		// e
+	// ))(document.createElement("label"));
+	// const label_h = (e=>(
+		// e.innerHTML = '高：<input type="number" min="5" step="1" value="15" placeholder="棋盘高度，例如 15" />',
+		// e
+	// ))(document.createElement("label"));
+	// const label_wincnt = (e=>(
+		// e.innerHTML = '胜利需要几子棋：<input type="number" min="5" step="1" value="5" placeholder="填 4 就是 四子棋" />',
+		// e
+	// ))(document.createElement("label"));
+	// const label_checker = (e=>(
+		// e.innerHTML = '胜利检查的方向：<input type="text" value="[[0,1],[1,1],[1,0],[1,-1]]" placeholder="以这个 [dx,dy] 列表检查" />',
+		// e
+	// ))(document.createElement("label"));
+	const button_init = (e=>(
+		e.innerHTML = '初始化',
+		e.addEventListener("click", event => {
+			res.chess.init(config.w, config.h, config.wincnt, config.checker);
+			res.chess.domUpdateAll(res.dom);
+		}),
+		e
+	))(document.createElement("label"));
+	document.body.appendChild((e=>(
+		e.style.border = "1px solid #888",
+		// e.appendChild((e=>e)(document.createElement("br"))),
+		e.appendChild((e=>(
+			e.innerText = JSON.stringify(config),
+			e
+		))(document.createElement("p")))
+		e.appendChild(button_init),
+		e.appendChild(p_status),
+		e.appendChild(p_winner),
+		e.appendChild(res.dom),
+		e
+	))(document.createElement("div")));
+});
